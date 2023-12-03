b5e35ec8adef57469a41e37a5fea0566a0c0deec
Priya Ranjan Kumar <116024984+ranjanrana1712@users.noreply.github.com>
2023-12-03 23:49:23 +0530

    update: html-transfered-into-js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// logic building Process
+
+// Input
+// processing
+// Output
+
+
+const initialPrice = document.querySelector('#intial-price');
+
+const stockQuantity = document.querySelector('#stocks-quantity');
+
+const currentPrice = document.querySelector('currentPrice');
+
+const submitBtn = document.querySelector('#button');
+
+const outputBox = document.querySelector('#output');
 
