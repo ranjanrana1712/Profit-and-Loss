51e593376e054853063101b05f709c62894d60bc
Priya Ranjan Kumar <116024984+ranjanrana1712@users.noreply.github.com>
2023-12-13 09:17:33 +0530

    update:function creation

--- script.js
+++ script.js
@@ -36,15 +36,31 @@ const outputBox = document.querySelector('#output');
 //     return lossPer;
 // }
 
+submitBtn.addEventListener('click' ,submitHandler);
+
+function submitHandler(){
+    const ip = Number(initialPrice.value);
+    const qty = Number(stockQuantity.value);
+    const curr = Number(currentPrice.value);
+    calculateProfitLoss(ip, qty, curr);
+}
+
 function calculateProfitLoss(initial, quantity, current) {
     if (initial > current) {
-        var loss = initial - current
+        var loss = (initial - current)*quantity;
+
+        var lossPer = (loss/current) * 100;
+
+        console.log(`hey the loss is ${loss} and the percent is ${lossPer}%`)
     }
     else if(initial < current){
-        var profit = current - initial;
+        var profit = (current - initial)*quantity;
+        var profitPer = (profit/current)*100;
+        console.log(`hey the profit is ${profit} and profit percentage is ${profitPer}%`)
     }
     else {
-        
+        console.log('No pain no gain no gain no pain');
 
     }
 }
+
